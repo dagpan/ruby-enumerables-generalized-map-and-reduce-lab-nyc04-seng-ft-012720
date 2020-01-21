60e8f445d474fe9defb2200b9bc04d4f5eb37090
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-21 20:30:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,8 +15,8 @@ end
 
 
 map_to_square(source_array) {||         }
-map_to_square(source_array) {||         }
-map_to_square(source_array) {||         }
+map_to_no_change(source_array) {||         }
+map_to_double(source_array) {||         }
 map_to_square(source_array) {||         }
 
 
