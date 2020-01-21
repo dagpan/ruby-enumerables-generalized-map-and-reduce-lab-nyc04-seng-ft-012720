cc8304ab2be2008f920a3c545fe0d5a34528764b
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-21 22:51:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,8 @@ def reduce(source_array, starting_point = 0)
     index += 1
   end
     if check == index
-       starting_point = nil
+       num = nil
+    elsif check 
     
     
   return_value = yield(num, starting_point)
@@ -41,7 +42,7 @@ end
 # reduce_to_total
 reduce([5,21,7,47]){|n, start| n + start}
 # map_to_all_true
-reduce([5,21,7,48]){|n| n}
+reduce([5,21,7,48]){|n, start| n && start}
 # map_to_any_true
-reduce([56,8,4,16]){|n| n + n}
+reduce([56,8,4,16]){|n, start| n && start}
 
