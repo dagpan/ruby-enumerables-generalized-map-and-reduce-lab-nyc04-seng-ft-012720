2fea5fff1d26148ba32790a4f4c936a9bf72b2eb
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-21 21:24:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,8 +5,8 @@ def map(source_array)
   result_array = []
   index = 0
   source_array.each do |num|
-    yield(num)
-    result_array[index] = num
+
+    result_array[index] = yield(num)
     index += 1
   end
   result_array
